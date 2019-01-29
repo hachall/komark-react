62b52e444768ee0c3be37132cc849c618999485f
Hamish Hall <hachall@hotmail.com>
2019-01-29 16:54:05 +0000

    index config

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
 
 const root = document.getElementById('root');
 if (root) {
