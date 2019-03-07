227feec8a8c92236e62f2f469663697a7e0c4eab
Hamish Hall <hachall@hotmail.com>
2019-03-07 18:35:59 +0000

    carousel mechanics working

--- src/containers/home_carousel.jsx
+++ src/containers/home_carousel.jsx
@@ -7,15 +7,93 @@ import {
   CarouselCaption
 } from 'reactstrap';
 
-
+const items = [
+  {
+    src: 'assets/images/lifex.jpeg',
+    title: 'Slide 1',
+    caption: 'Slide 1',
+    link: null,
+    link_url: '/',
+    pos: "left"
+  }
+];
 
 class HomeCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
 
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
 
   render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+
+      const posClass = `carousel-item-info carousel-info-${item.pos}`
+
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.caption}
+          slide= {false}
+        >
+          <img className="home-carousel-image" src={item.src} alt={item.altText} />
+          <div className={posClass}>
+            <div className="">
+              <h3 className="carousel-text">{item.title}</h3>
+            </div>
+            <div className="">
+              <p className="carousel-text">{item.caption}</p>
+            </div>
+            {item.link ? <a href={item.link_url} className="carousel-text">{item.link}</a> : ""}
+          </div>
+        </CarouselItem>
+      );
+    });
 
     return (
-      <img className="home-carousel" src="assets/images/lifex.jpeg" alt=""/>
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
     );
   }
 }
